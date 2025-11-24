76240a60e1afa65e2ea355ca1bc5795c02cd91f6
Nishu224 <nishadibatagalla08@gmail.com>
2025-11-24 18:23:40 +0530

    Errors making

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,6 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.log(err));
 
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
@@ -23,8 +22,6 @@ import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
-
-const app = express();
 
 // Connect to database
 connectDB();
